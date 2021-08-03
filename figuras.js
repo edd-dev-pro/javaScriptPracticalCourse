86bb6bc70a19469e00de73da17e84404397731d9
eduardo <soka_117@live.com>
2021-08-03 16:01:38 -0500

    Quinto commit / Añadi constantes y mensajes en consola

--- figuras.js
+++ figuras.js
@@ -1,1 +1,25 @@
-console.log('hellow word')
+// Código del cuadrado
+const ladoCuadrado = 5
+console.log('Los lados del cuadrado miden: ' + ladoCuadrado + 'cm')
+
+const perimetroCuadrado = ladoCuadrado * 4
+console.log('El perímetro del cuadrado es: ' + perimetroCuadrado + 'cm')
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado
+console.log('El área del cuadrado es: ' + areaCuadrado + 'cm^2')
+
+// Código del triángulo
+const ladoTriangulo1 = 6
+const ladoTriangulo2 = 6
+const ladoTrianguloB = 4
+
+// console.log('Los lados del triángulo miden ' + ladoTriangulo1 + 'cm, ' + ladoTriangulo2 + 'cm, ' + ladoTrianguloB + 'cm')
+console.log(
+    'Los lados del triángulo miden ' 
+    + ladoTriangulo1 
+    + 'cm, ' 
+    + ladoTriangulo2 
+    + 'cm, ' 
+    + ladoTrianguloB 
+    + 'cm'
+)
