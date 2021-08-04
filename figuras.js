dd9be64fcd83e0bc850c46143f465baa85100ecf
eduardo <soka_117@live.com>
2021-08-03 20:46:17 -0500

    Octavo commit / Comverti constantes en funciones

--- figuras.js
+++ figuras.js
@@ -1,50 +1,51 @@
 // Código del cuadrado
 console.group('Cuadrados') // creamos un grupo de mensajes
-const ladoCuadrado = 5
-console.log('Los lados del cuadrado miden: ' + ladoCuadrado + 'cm')
+// const ladoCuadrado = 5
+// console.log('Los lados del cuadrado miden: ' + ladoCuadrado + 'cm')
 
-const perimetroCuadrado = ladoCuadrado * 4
-console.log('El perímetro del cuadrado es: ' + perimetroCuadrado + 'cm')
+function perimetroCuadrado(lado) {
+    return lado * 4
+}
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado
-console.log('El área del cuadrado es: ' + areaCuadrado + 'cm^2')
+function areaCuadrado(lado) {
+    return lado * lado
+}
 console.groupEnd()
 
 // Código del triángulo
 console.group('Tríangulos')
-const ladoTriangulo1 = 6
-const ladoTriangulo2 = 6
-const ladoTrianguloB = 4
-console.log(
-    'Los lados del triángulo miden ' 
-    + ladoTriangulo1 
-    + 'cm, ' 
-    + ladoTriangulo2 
-    + 'cm, ' 
-    + ladoTrianguloB 
-    + 'cm'
-)
+// const ladoTriangulo1 = 6
+// const ladoTriangulo2 = 6
+// const ladoTrianguloB = 4
+// console.log(
+//     'Los lados del triángulo miden ' 
+//     + ladoTriangulo1 
+//     + 'cm, ' 
+//     + ladoTriangulo2 
+//     + 'cm, ' 
+//     + ladoTrianguloB 
+//     + 'cm'
+// )
 
-const alturaTriangulo = 5.5
-console.log('La altura del tríangulo es de: ' + alturaTriangulo + 'cm')
+// const alturaTriangulo = 5.5
+// console.log('La altura del tríangulo es de: ' + alturaTriangulo + 'cm')
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloB
-console.log ('El perímetro del tríangulo es: ' + perimetroTriangulo + 'cm')
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base
+}
 
-const areaTriangulo = (ladoTrianguloB * alturaTriangulo) / 2 // Con los parentesis indicamos que esa operacion s resueve primero
-console.log('El área del tríangulo es: ' + areaTriangulo + 'cm^2')
+function areaTriangulo(base, altura) {
+    return (base * altura) / 2
+}
+// const areaTriangulo = (ladoTrianguloB * alturaTriangulo) / 2 // Con los parentesis indicamos que esa operacion s resueve primero
 console.groupEnd()
 
 // Código del círculo
 console.group('Círculo')
-
-// Radio
-const radioCirculo = 4
-console.log('El radio del círculo es: ' + radioCirculo + 'cm')
-
 // Diámetro
-const diametroCirculo = radioCirculo * 2
-console.log('El díametro del círculo es: ' + diametroCirculo + 'cm')
+function diametroCirculo(radio) {
+    return radio * 2
+}
 
 // PI
 // const PI = 3.1416
@@ -52,10 +53,13 @@ const PI = Math.PI // Calculamos Pi con Math
 console.log('PI es: ' + PI)
 
 // Circunferencia
-const perimetroCirculo = diametroCirculo * PI
-console.log('El perímetro del círculo es: ' + perimetroCirculo + 'cm')
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio)
+    return diametro * PI
+}
 
 // Área
-const areaCirculo = (radioCirculo * radioCirculo) * PI
-console.log('El área del círculo es: ' + areaCirculo + 'cm^2')
+function areaCirculo(radio, ) {
+    return (radio * radio) * PI
+}
 console.groupEnd()
