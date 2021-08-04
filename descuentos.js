d05f0c1632a078b01fa47127e1f347a1db32c8ca
eduardo <soka_117@live.com>
2021-08-04 00:04:41 -0500

    Onceavo commit / Se creo la estructura del HTML y las funciones de JavaScript para crear la calculadora

--- descuentos.js
+++ descuentos.js
@@ -14,3 +14,15 @@ function calcularPrecioConDescuento(precio, descuento) {
 //     precioConDescuento
 // }) // Imprmimos los valores en consola desde un objeto.
 
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById('InputPrice')
+    const priceValue = inputPrice.value
+    
+    const inputDiscount = document.getElementById('InputDiscount')
+    const discountValue = inputDiscount.value
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue)
+    
+    const resultP = document.getElementById('ResultP')
+    resultP.innerText = 'El precio con descuento son: $' + precioConDescuento
+}
